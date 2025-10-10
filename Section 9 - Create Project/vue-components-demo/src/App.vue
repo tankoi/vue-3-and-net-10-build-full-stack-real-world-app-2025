a321c5ff7a02d26ca321a792b4752104dc8585b0
nameless <tistvan1989@gmail.com>
2025-10-10 19:00:42 +0300

    72. Vue in action

--- Section 9 - Create Project/vue-components-demo/src/App.vue
+++ Section 9 - Create Project/vue-components-demo/src/App.vue
@@ -1,3 +1,24 @@
 <template>
-    <div>Hello World!</div>
+    <div>{{ message }}</div>
 </template>
+
+<script>
+
+export default {
+    setup() {
+        // Composition API code can go here
+        const message = "Hello from Composition API!";
+
+        return {
+            message
+        }
+    }
+}
+
+</script>
+
+<style>
+
+
+
+</style>
