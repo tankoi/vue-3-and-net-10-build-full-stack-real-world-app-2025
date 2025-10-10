4b08dd9bd2d11fdafd7a2fa86e59ba803bf55834
nameless <tistvan1989@gmail.com>
2025-10-10 19:43:45 +0300

    76. Local vs global components

--- Section 9 - Create Project/vue-components-demo/src/App.vue
+++ Section 9 - Create Project/vue-components-demo/src/App.vue
@@ -1,10 +1,14 @@
 <template>
     <div>{{ message }}</div>
     <contact-us></contact-us>
+    <!-- local component -->
+    <button-counter></button-counter>
     <button-counter></button-counter>
 </template>
 
 <script setup>
+import ButtonCounter from './components/ButtonCounter.vue';
+
 const message = "Hello from Composition API!";
 </script>
 
--- Section 9 - Create Project/vue-components-demo/src/main.js
+++ Section 9 - Create Project/vue-components-demo/src/main.js
@@ -1,11 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ContactUs from './components/ContactUs.vue'
-import ButtonCounter from './components/ButtonCounter.vue'
 
 const app = createApp(App);
-
+// global component
 app.component('contact-us', ContactUs);
-app.component('button-counter', ButtonCounter);
 
 app.mount('#app');
