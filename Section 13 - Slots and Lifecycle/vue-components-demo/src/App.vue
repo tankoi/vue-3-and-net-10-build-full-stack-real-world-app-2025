a25530a05c125760b58b388c7579da7483e69280
nameless <tistvan1989@gmail.com>
2025-10-24 18:57:35 +0300

    103. Dynamic Components

--- Section 13 - Slots and Lifecycle/vue-components-demo/src/App.vue
+++ Section 13 - Slots and Lifecycle/vue-components-demo/src/App.vue
@@ -2,16 +2,27 @@
   <div class="bg-black text pt-3" :style="{ height: '100vh' }">
     <h1 class="text-center text-success">ContactOpedia</h1>
     <div class="container">
-      <LuckyNumber></LuckyNumber>
+      <button class="btn btn-primary text-black m-2" @click="newVersion = !newVersion">Toggle Component</button>
+      <button class="btn btn-primary text-black m-2" @click="newVersion = false">Lucky Number V1</button>
+      <button class="btn btn-primary text-black m-2" @click="newVersion = true">Lucky Number V2</button>
       <br>
-      <LuckyNumberV2></LuckyNumberV2>
+      <br>
+      <component :is="currentComponent" />
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref, computed } from 'vue';
 import LuckyNumber from './components/LuckyNumber.vue';
 import LuckyNumberV2 from './components/LuckyNumberV2.vue';
+
+const newVersion = ref(false);
+
+const currentComponent = computed(() => {
+  return newVersion.value ? LuckyNumberV2 : LuckyNumber;
+});
+
 </script>
 
 <style></style>
