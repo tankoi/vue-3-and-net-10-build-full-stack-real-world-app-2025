b7a444bc3b5b54b9f61c42f98e19a43aa81be1fd
István Tankó <istvan_tanko@yahoo.com>
2025-10-24 15:38:38 +0300

    97. Lucky Number Generator

--- Section 9 - Create Project/vue-components-demo/src/components/Contact.vue
+++ Section 9 - Create Project/vue-components-demo/src/components/Contact.vue
@@ -1,46 +1,61 @@
 <template>
-    <div class="bg-info rounded p-1 pb-4 m-1">
-        <div class="row">
-            <div class="col-6">
-                <h3>Name: {{ name }}</h3>
-                <p>Email: {{ email }}</p>
-                <p>Phone: {{ phone }}</p>
-            </div>
-            <div class="col-3">
-                <button
-                    @click="emit('update-favorite', { isFavorite: props.isFavorite, name: props.name });"
-                    :class="[isFavorite ? 'btn btn-warning' : 'btn btn-success']">
-                    {{ isFavorite ? 'Remove from Favorites' : 'Add to Favorites' }}
-                </button>
-            </div>
-        </div>
-        <span class="float-end small" v-if="ownerName != ''">
-            *this contact information belongs to {{ ownerName }}
-        </span>
+  <div class="bg-info rounded p-1 pb-4 m-1">
+    <div class="row">
+      <div class="col-5">
+        <h3>Name: {{ name }}</h3>
+        <p>Email: {{ email }}</p>
+        <p>Phone: {{ phone }}</p>
+      </div>
+      <div class="col-3">
+        <button
+          @click="
+            emit('update-favorite', {
+              isFavorite: props.isFavorite,
+              name: props.name,
+            })
+          "
+          :class="[
+            isFavorite
+              ? 'btn btn-warning form-control'
+              : 'btn btn-success form-control',
+          ]"
+        >
+          {{ isFavorite ? "Remove from Favorites" : "Add to Favorites" }}
+        </button>
+      </div>
+      <div class="col-4">
+        <lucky-number :maxNumber="10"></lucky-number>
+      </div>
     </div>
+    <span class="float-end small" v-if="ownerName != ''">
+      *this contact information belongs to {{ ownerName }}
+    </span>
+  </div>
 </template>
 
 <script setup>
+import LuckyNumber from "./LuckyNumber.vue";
+
 const props = defineProps({
-    name: {
-        type: String,
-        required: true,
-    },
-    phone: {
-        type: Number
-    },
-    ownerName: {
-        type: String
-    },
-    email: {
-        type: String,
-        required: false,
-        default: "-n/a-"
-    },
-    isFavorite: {
-        type: Boolean
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: Number,
+  },
+  ownerName: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: false,
+    default: "-n/a-",
+  },
+  isFavorite: {
+    type: Boolean,
+  },
 });
 
-const emit = defineEmits(['update-favorite']);
+const emit = defineEmits(["update-favorite"]);
 </script>
