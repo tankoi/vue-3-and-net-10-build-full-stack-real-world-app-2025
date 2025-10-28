02d718bf161f71b4f4f62595fe3d187782761657
nameless <tistvan1989@gmail.com>
2025-10-28 20:03:34 +0200

    109. Named Slots

--- Section 13 - Slots and Lifecycle/vue-components-demo/src/components/LuckyNumberParentComponent.vue
+++ Section 13 - Slots and Lifecycle/vue-components-demo/src/components/LuckyNumberParentComponent.vue
@@ -4,9 +4,12 @@
 		<div class="container text-center bg-white">
 			<slot></slot>
 			<button class="btn btn-primary text-black m-2" @click="newVersion = !newVersion">Toggle Component</button>
+			<slot name="moreInfo"></slot>
+			<br>
 			<button class="btn btn-primary text-black m-2" @click="newVersion = false">Lucky Number V1</button>
 			<button class="btn btn-primary text-black m-2" @click="newVersion = true">Lucky Number V2</button>
 			<br>
+			<slot name="learnSlot"></slot>
 			<br>
 			<!-- component will not be remounted, so it preserves its state -->
 			<keep-alive :include="['LuckyNumber', 'LuckyNumberV2']">
