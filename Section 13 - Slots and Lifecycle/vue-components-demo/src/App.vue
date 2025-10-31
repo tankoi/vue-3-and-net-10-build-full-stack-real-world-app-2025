467fe113583b62932c7f08f2cd69135a35f0e768
István Tankó <istvan_tanko@yahoo.com>
2025-10-31 15:27:55 +0200

    112. Lifecycle Demo

--- Section 13 - Slots and Lifecycle/vue-components-demo/src/App.vue
+++ Section 13 - Slots and Lifecycle/vue-components-demo/src/App.vue
@@ -1,7 +1,46 @@
 <template>
-  <h1>Hello Vue</h1>
+  <h1>{{ message }}</h1>
+  <button @click="showMessage">Show Message</button>
+  <br />
+  <h1>{{ count }}</h1>
+  <button @click="count++">Clicked {{ count }} times</button>
+  <ButtonCounter v-if="count == 1" />
 </template>
 
 <script setup>
+import ButtonCounter from "./components/ButtonCounter.vue";
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from "vue";
 
+const message = ref("Hello Vue");
+const showMessage = () => {
+  message.value = "Hello Vue from function";
+};
+const count = ref(0);
+
+onBeforeMount(() => {
+  console.log("onBeforeMount - App.vue");
+});
+onMounted(() => {
+  console.log("onMounted - App.vue");
+});
+onBeforeUpdate(() => {
+  console.log("onBeforeUpdate - App.vue");
+});
+onUpdated(() => {
+  console.log("onUpdated - App.vue");
+});
+onBeforeUnmount(() => {
+  console.log("onBeforeUnmount - App.vue");
+});
+onUnmounted(() => {
+  console.log("onUnmounted - App.vue");
+});
 </script>
--- Section 13 - Slots and Lifecycle/vue-components-demo/src/components/ButtonCounter.vue
+++ Section 13 - Slots and Lifecycle/vue-components-demo/src/components/ButtonCounter.vue
@@ -1,8 +1,36 @@
 <template>
-    <button @click="count++">Clicked {{ count }} times</button>
+  <button @click="count++">Clicked {{ count }} times</button>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import {
+  ref,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from "vue";
+
 const count = ref(0);
+
+onBeforeMount(() => {
+  console.log("onBeforeMount - ButtonCounter.vue");
+});
+onMounted(() => {
+  console.log("onMounted - ButtonCounter.vue");
+});
+onBeforeUpdate(() => {
+  console.log("onBeforeUpdate - ButtonCounter.vue");
+});
+onUpdated(() => {
+  console.log("onUpdated - ButtonCounter.vue");
+});
+onBeforeUnmount(() => {
+  console.log("onBeforeUnmount - ButtonCounter.vue");
+});
+onUnmounted(() => {
+  console.log("onUnmounted - ButtonCounter.vue");
+});
 </script>
