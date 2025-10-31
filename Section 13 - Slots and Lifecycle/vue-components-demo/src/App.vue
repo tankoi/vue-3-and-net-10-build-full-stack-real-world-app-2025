f92aeed7d36fde2a16fb56c640f94d20bc279f24
István Tankó <istvan_tanko@yahoo.com>
2025-10-31 15:07:52 +0200

    111. Lifecycle cleanup

--- Section 13 - Slots and Lifecycle/vue-components-demo/src/App.vue
+++ Section 13 - Slots and Lifecycle/vue-components-demo/src/App.vue
@@ -1,27 +1,7 @@
 <template>
   <h1>Hello Vue</h1>
-  <LuckyNumberParentComponent>
-    <template v-slot:default>
-      <p class="pt-2">We have two version for picking lucky numbers</p>
-      <hr>
-    </template>
-    <template v-slot:moreInfo>
-      <p>Click the buttons above to switch between versions.</p>
-    </template>
-    <template v-slot:learnSlot>
-      <button @click="showMessage">What will we learn?</button>
-      <h4 class="text-success">{{ message }}</h4>
-    </template>
-  </LuckyNumberParentComponent>
 </template>
 
 <script setup>
-import LuckyNumberParentComponent from './components/LuckyNumberParentComponent.vue';
-import { ref } from 'vue';
 
-const message = ref('');
-
-function showMessage() {
-  message.value = 'We will learn how to use slots';
-}
 </script>
