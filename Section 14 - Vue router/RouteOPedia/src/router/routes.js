60514f35aecb2fbe119e1da6827e606cfd484360
nameless <tistvan1989@gmail.com>
2025-11-07 17:07:40 +0200

    121. Routing in action

--- Section 14 - Vue router/RouteOPedia/src/router/routes.js
+++ Section 14 - Vue router/RouteOPedia/src/router/routes.js
@@ -1,8 +1,16 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "@/components/Home/HomePage.vue";
+import Contact from "@/components/Home/Contact.vue";
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [{}],
+	history: createWebHistory(),
+	routes: [{
+		path: "/",
+		component: HomePage,
+	}, {
+		path: "/contact",
+		component: Contact,
+	}],
 });
 
 export default router;
