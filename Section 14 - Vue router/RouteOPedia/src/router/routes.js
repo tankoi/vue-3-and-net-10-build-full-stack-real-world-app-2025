7ec94e59cd99a4f1df8c2b902c18ec6bfef8bae6
nameless <tistvan1989@gmail.com>
2025-11-07 17:11:12 +0200

    122. Vite Configuration

--- Section 14 - Vue router/RouteOPedia/src/router/routes.js
+++ Section 14 - Vue router/RouteOPedia/src/router/routes.js
@@ -3,7 +3,7 @@ import HomePage from "@/components/Home/HomePage.vue";
 import Contact from "@/components/Home/Contact.vue";
 
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHistory(import.meta.env.BASE_URL),
 	routes: [{
 		path: "/",
 		component: HomePage,
