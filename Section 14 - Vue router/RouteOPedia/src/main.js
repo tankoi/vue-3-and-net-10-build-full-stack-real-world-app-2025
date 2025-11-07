1acfbfaea656a332860d34ed325fd0dd84db75b8
nameless <tistvan1989@gmail.com>
2025-11-07 17:50:12 +0200

    123. Add header

--- Section 14 - Vue router/RouteOPedia/src/main.js
+++ Section 14 - Vue router/RouteOPedia/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/routes.js";
+// Import Bootstrap CSS and JS
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 const app = createApp(App);
 
