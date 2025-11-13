59b5354f85919468173830b744fa88e1c670d3ae
nameless <tistvan1989@gmail.com>
2025-11-13 16:58:26 +0200

    132. Navigate from script to another component

--- Section 14 - Vue router/RouteOPedia/src/components/Home/HomePage.vue
+++ Section 14 - Vue router/RouteOPedia/src/components/Home/HomePage.vue
@@ -1,3 +1,16 @@
 <template>
   <div>Home page</div>
+  <button @click="goToContact">Go to Contact Page</button>
 </template>
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+function goToContact() {
+  //router.push({ name: 'contact' });
+  router.push('/contact');
+}
+
+</script>
