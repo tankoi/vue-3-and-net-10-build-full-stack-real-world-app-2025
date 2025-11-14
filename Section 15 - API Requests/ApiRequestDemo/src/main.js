695b05912e88a86e17927cca7156a0fa3a9078ba
nameless <tistvan1989@gmail.com>
2025-11-14 18:29:06 +0200

    138. Create Project and cleanup

---
+++ Section 15 - API Requests/ApiRequestDemo/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+// Import Bootstrap CSS and JS
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+createApp(App).mount('#app')
