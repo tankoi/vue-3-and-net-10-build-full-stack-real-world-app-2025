2e9a2ab4e69abf0c3e0ce5e32d9aec71bf2a32ba
nameless <tistvan1989@gmail.com>
2025-11-26 17:56:50 +0200

    150. UI for Home Page

--- Section 16 - Pinia/PiniaDemo/PiniaDemo/src/components/Home.vue
+++ Section 16 - Pinia/PiniaDemo/PiniaDemo/src/components/Home.vue
@@ -1,3 +1,19 @@
 <template>
-    Home
+    <div class="text-center">
+        <p>Let's play a game</p>
+        <h2 class="text-primary pb-3">Current Score</h2>
+        <span class="text-primary pb-3">Max Score:</span>
+        <br>
+        <div class="row">
+            <div class="col-5 offset-1">
+                <button class="btn btn-success form-control p-4">Increase</button>
+            </div>
+            <div class="col-5">
+                <button class="btn btn-danger form-control p-4">Decrease</button>
+            </div>
+            <div class="col-6 offset-3 pt-2">
+                <button class="btn btn-warning form-control p-4">Random</button>
+            </div>
+        </div>
+    </div>
 </template>
