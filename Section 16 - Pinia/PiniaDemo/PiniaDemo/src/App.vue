2c32da91025c92ce45dcb1d1ef6b9cefac74ead1
nameless <tistvan1989@gmail.com>
2025-11-26 17:42:50 +0200

    147. Routing in action

--- Section 16 - Pinia/PiniaDemo/PiniaDemo/src/App.vue
+++ Section 16 - Pinia/PiniaDemo/PiniaDemo/src/App.vue
@@ -2,7 +2,16 @@
 
 <template>
   <header>
-    <div class="wrapper">Hello World!</div>
+    <div class="text-ceter pt-3 text-success h3">CountOPedia</div>
+    <hr>
+    <div class="row">
+      <div class="col-4 offset-1">
+        <RouterLink class="form-control badge rounded-pill text-bg-warning p-4" to="/">Home</RouterLink>
+      </div>
+      <div class="col-4 offset-1">
+        <RouterLink class="form-control badge rounded-pill text-bg-warning p-4" to="/info">Info</RouterLink>
+      </div>
+    </div>
   </header>
   <RouterView />
 </template>
