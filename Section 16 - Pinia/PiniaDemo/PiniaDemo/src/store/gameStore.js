56bc234507efe674f3ea1cad5d995c7ae74ee1d4
István Tankó <istvan_tanko@yahoo.com>
2025-11-28 15:48:34 +0200

    157. Composition API Style

--- Section 16 - Pinia/PiniaDemo/PiniaDemo/src/store/gameStore.js
+++ Section 16 - Pinia/PiniaDemo/PiniaDemo/src/store/gameStore.js
@@ -1,33 +1,41 @@
 import { defineStore } from "pinia";
+import { ref, computed } from "vue";
 
-export const useGameStore = defineStore("gameStore", {
-    state: () => ({
-        score: 50,
-        maxHealth: 100,
-        maxAttack: 30,
-        maxDefense: 10,
-    }),
-    getters: {
-        getScore() {
-            return this.score;
-        },
-        getWinningScore() {
-            return this.maxHealth;
-        }
-    },
-    actions: {
-        setNextAttack() {
-            let attack = Math.floor(Math.random() * this.maxAttack) + 1;
-            console.log("attack", attack);
-            this.score += attack;
-        },
-        setNextDefense() {
-            let defense = Math.floor(Math.random() * this.maxDefense) + 1;
-            console.log("defense", defense);
-            this.score -= defense;
-        },
-        resetScore() {
-            this.score = 50;
-        }
-    }
+export const useGameStore = defineStore("gameStore", () => {
+  const score = ref(50);
+  const maxHealth = ref(100);
+  const maxAttack = ref(30);
+  const maxDefense = ref(10);
+
+  const getScore = computed(() => score.value);
+  const getWinningScore = computed(() => maxHealth.value);
+
+  const setNextAttack = () => {
+    let attack = Math.floor(Math.random() * maxAttack.value) + 1;
+    console.log("attack", attack);
+    score.value += attack;
+  };
+  const setNextDefense = () => {
+    let defense = Math.floor(Math.random() * maxDefense.value) + 1;
+    console.log("defense", defense);
+    score.value -= defense;
+  };
+  const resetScore = () => {
+    score.value = 50;
+  };
+
+  return {
+    // state
+    score,
+    maxHealth,
+    maxAttack,
+    maxDefense,
+    // getters
+    getScore,
+    getWinningScore,
+    // actions
+    setNextAttack,
+    setNextDefense,
+    resetScore,
+  };
 });
