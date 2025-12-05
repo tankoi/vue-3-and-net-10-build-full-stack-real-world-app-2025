4233996655cf0ca8191e88b5596695a94811fd7e
István Tankó <istvan_tanko@yahoo.com>
2025-12-05 15:46:33 +0200

    160. Add Scalar

--- Section 17 - API setup/MangoFusion_API/Program.cs
+++ Section 17 - API setup/MangoFusion_API/Program.cs
@@ -1,21 +1,24 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.MapOpenApi();
+	app.MapScalarApiReference();
+}
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.Run();
