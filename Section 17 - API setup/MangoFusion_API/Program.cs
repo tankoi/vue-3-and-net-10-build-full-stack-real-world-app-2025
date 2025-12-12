77f88233b572a675bc179ad5fe0024f7442ae962
István Tankó <istvan_tanko@yahoo.com>
2025-12-12 15:42:21 +0200

    162. Create Identity Tables

--- Section 17 - API setup/MangoFusion_API/Program.cs
+++ Section 17 - API setup/MangoFusion_API/Program.cs
@@ -1,8 +1,18 @@
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using MyApp.Data;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+	options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+	.AddEntityFrameworkStores<ApplicationDbContext>()
+	.AddDefaultTokenProviders();
+
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
