66bf0e5aaa6d77eb06442dc4cdd327ae9bd032aa
nameless <tistvan1989@gmail.com>
2025-12-16 16:56:20 +0200

    167. Get All Menu Items

---
+++ Section 17 - API setup/MangoFusion_API/Controllers/MenuItemController.cs
@@ -0,0 +1,31 @@
+using MangoFusion_API.Data;
+using MangoFusion_API.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace MangoFusion_API.Controllers
+{
+    [ApiController]
+    [Route("api/MenuItem")]
+    public class MenuItemController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        private readonly ApiResponse _response;
+
+        public MenuItemController(ApplicationDbContext db)
+        {
+            _db = db;
+            _response = new ApiResponse();
+        }
+
+        [HttpGet]
+
+        public IActionResult GetMenuItems()
+        {
+            _response.Result = _db.MenuItems.ToList();
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+    }
+}
