223fbabf0188ce60d8b15cfe9c8608590d2845bd
nameless <tistvan1989@gmail.com>
2025-12-16 17:18:34 +0200

    168. Get Menu Item by Id

--- Section 17 - API setup/MangoFusion_API/Controllers/MenuItemController.cs
+++ Section 17 - API setup/MangoFusion_API/Controllers/MenuItemController.cs
@@ -20,10 +20,24 @@ namespace MangoFusion_API.Controllers
         }
 
         [HttpGet]
-
         public IActionResult GetMenuItems()
         {
             _response.Result = _db.MenuItems.ToList();
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+
+        [HttpGet("{id:int}", Name = "GetMenuItem")]
+        public IActionResult GetMenuItem(int id)
+        {
+            if (id == 0)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                return BadRequest(_response);
+            }
+            MenuItem? menuItem = _db.MenuItems.FirstOrDefault(u => u.Id == id);
+            _response.Result = menuItem;
             _response.StatusCode = HttpStatusCode.OK;
             return Ok(_response);
         }
