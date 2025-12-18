acba25d65649e493aaf9445ca4769750c5d97aa7
nameless <tistvan1989@gmail.com>
2025-12-18 09:58:36 +0200

    169. DTOs for Create and Update Menu Item

---
+++ Section 17 - API setup/MangoFusion_API/Models/Dto/MenuItemCreateDTO.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MangoFusion_API.Models.Dto
+{
+    public class MenuItemCreateDTO
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Category { get; set; } = string.Empty;
+
+        public string SpecialTag { get; set; } = string.Empty;
+
+        [Range(1, 10000)]
+        public double Price { get; set; }
+        
+        public IFormFile File { get; set; } = null!;
+    }
+}
---
+++ Section 17 - API setup/MangoFusion_API/Models/Dto/MenuItemUpdateDTO.cs
@@ -0,0 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MangoFusion_API.Models.Dto
+{
+    public class MenuItemUpdateDTO
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public string Category { get; set; } = string.Empty;
+
+        public string SpecialTag { get; set; } = string.Empty;
+
+        [Range(1, 10000)]
+        public double Price { get; set; }
+
+        public IFormFile File { get; set; } = null!;
+    }
+}
