ea1f9540d4ad789a4b62968c4adfcff971de7368
nameless <tistvan1989@gmail.com>
2025-12-18 10:07:34 +0200

    171. Part 3 Update Menu Item

--- Section 17 - API setup/MangoFusion_API/Models/MenuItem.cs
+++ Section 17 - API setup/MangoFusion_API/Models/MenuItem.cs
@@ -10,11 +10,11 @@ namespace MangoFusion_API.Models
         [Required]
         public string Name { get; set; } = string.Empty;
 
-        public string Description { get; set; } = string.Empty;
-
+        public string? Description { get; set; }
+        
         public string Category { get; set; } = string.Empty;
 
-        public string SpecialTag { get; set; } = string.Empty;
+        public string? SpecialTag { get; set; }
 
         [Range(1, 10000)]
         public double Price { get; set; }   
