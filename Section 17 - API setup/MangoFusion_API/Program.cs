15d14ce30d6cfbbc934632f4f39d66b90dfbc02e
István Tankó <istvan_tanko@yahoo.com>
2026-01-23 16:07:30 +0200

    179. Configure Authentication in API

--- Section 17 - API setup/MangoFusion_API/Program.cs
+++ Section 17 - API setup/MangoFusion_API/Program.cs
@@ -1,21 +1,42 @@
+using System.Text;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 using MangoFusion_API.Data;
 using Microsoft.AspNetCore.Identity;
 using MangoFusion_API.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 	options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
 	.AddEntityFrameworkStores<ApplicationDbContext>()
 	.AddDefaultTokenProviders();
-
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+var key = builder.Configuration.GetValue<string>("ApiSettings:Secret");
+builder.Services.AddAuthentication(configureOptions =>
+{
+	configureOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	configureOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(jwtBearerOptions =>
+{
+	jwtBearerOptions.RequireHttpsMetadata = false;
+	jwtBearerOptions.SaveToken = true;
+	jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
+	{
+		ValidateIssuerSigningKey = true,
+		ValidateIssuer = false,
+		ValidateAudience = false,
+		IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+	};
+});
 
 var app = builder.Build();
 
@@ -25,12 +46,11 @@ if (app.Environment.IsDevelopment())
 	app.MapOpenApi();
 	app.MapScalarApiReference();
 }
+
 app.UseDefaultFiles();
 app.UseStaticFiles();
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
