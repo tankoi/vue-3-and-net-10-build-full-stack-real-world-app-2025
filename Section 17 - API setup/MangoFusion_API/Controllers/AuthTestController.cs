47d2f2f05f412a9d5a7819d0cb13514f727d14e9
István Tankó <istvan_tanko@yahoo.com>
2026-01-23 15:50:44 +0200

    178. Add Auth Test Controller

---
+++ Section 17 - API setup/MangoFusion_API/Controllers/AuthTestController.cs
@@ -0,0 +1,25 @@
+﻿using MangoFusion_API.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MangoFusion_API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class AuthTestController
+	{
+		[HttpGet]
+		[Authorize]
+		public ActionResult<string> GetSomething()
+		{
+			return "You are authorized user";
+		}
+		
+		[HttpGet("{someValue:int}")]
+		[Authorize(Roles = SD.Role_Admin)]
+		public ActionResult<string> GetSomething(int someValue)
+		{
+			return "You are authorized user with role of Admin";
+		}
+	}
+}
---
+++ Section 17 - API setup/MangoFusion_API/Utility/SD.cs
@@ -0,0 +1,11 @@
+﻿namespace MangoFusion_API.Utility
+{
+	/// <summary>
+	/// Static Details class for storing constant values such as user roles.
+	/// </summary>
+	public static class SD
+	{
+		public const string Role_Admin = "Admin";
+		public const string Role_Customer = "Customer";
+	}
+}
