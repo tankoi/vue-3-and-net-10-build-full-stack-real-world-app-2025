b18c6e3a1f7e245f9cd2e8038de5dc61b4e28bc9
István Tankó <istvan_tanko@yahoo.com>
2026-01-30 13:40:14 +0200

    182. Login and Register DTO

---
+++ Section 17 - API setup/MangoFusion_API/Models/Dto/RegisterRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MangoFusion_API.Models.Dto;
+
+public class RegisterRequestDTO
+{
+	[Required]
+	public string Email { get; set; } = string.Empty;
+	
+	[Required]
+	public string Password { get; set; } = string.Empty;
+	
+	[Required]
+	public string ConfirmPassword { get; set; } = string.Empty;
+	
+	public string? Role { get; set; } = string.Empty;
+}
